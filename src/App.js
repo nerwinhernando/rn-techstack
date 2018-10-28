028af40c3cdbc6538edc3880ebb5f1d59528f478
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-28 16:22:08 +0800

    render Header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
     return(
         <Provider store={createStore(reducers)}>
             <View>
-                <Text>Hellollooolll</Text>
+                <Header headerText="Tech Stack" />
             </View>
          </Provider>
     );
