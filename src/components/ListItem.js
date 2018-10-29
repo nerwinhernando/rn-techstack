f54296a0b2a4de4d32766b8e0e6de97f3b3c788a
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-30 01:15:28 +0800

    Animations

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,27 @@
 import React from 'react';
-import { Text, View, TouchableWithoutFeedback } from 'react-native';
+import { 
+    Text, 
+    View, 
+    TouchableWithoutFeedback,
+    LayoutAnimation 
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends React.Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring()
+    }
+
     renderDescription() {
         const { library, expanded } = this.props;
 
         if(expanded) {
             return (
-                <Text>{library.description}</Text>
+                <CardSection>
+                    <Text style={{flex: 1}}>{library.description}</Text>
+                </CardSection>
             );
         }
     }
