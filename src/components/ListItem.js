c25c2eac0ddb085426a87e22a66a2f0bd67da6b6
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-30 00:26:19 +0800

    Scrolling List and style

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,12 +4,22 @@ import { CardSection } from './common';
 
 class ListItem extends React.Component {
     render() {
+        const { titleStyle } = styles;
         //console.log(this.props.library)
         return(
             <CardSection>
-                <Text>{this.props.library.title}</Text>
+                <Text style={titleStyle}>
+                    {this.props.library.title}
+                </Text>
             </CardSection>
         );
+    }
+};
+
+const styles = {
+    titleStyle: {
+        fontSize: 18,
+        paddingLeft: 15
     }
 };
 
