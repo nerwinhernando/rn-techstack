d465a37401fb617d8e8adfbf8b29c264b0784476
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-30 00:55:42 +0800

    select_library implementation

--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,4 +1,9 @@
-export default (state, action) => {
-    console.log(action);
-    return null;
+// don't forget to set the state to null because this will throw undefined
+export default (state = null, action) => {
+    switch(action.type) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
 };
