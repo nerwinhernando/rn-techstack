131a73ec338817fed65a922843666c1288101069
Nerwin Hernando <nerwinc.hernando@gmail.com>
2018-10-30 00:17:42 +0800

    Fu*** LibraryList

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+import ListItem from './ListItem';
 
 class LibraryList extends React.Component {
+    renderItem({item}) {
+        return <ListItem library={item} />;
+    }
+
     render() {
-        return;
+        return(
+            <FlatList 
+                data={this.props.libraries}
+                renderItem={this.renderItem}
+                //renderItem={({item}) => <ListItem library={item} />}
+                keyExtractor={library => library.id}
+            />
+        );
     }
 }
+
+{/* <FlatList
+  data={[{title: 'Title Text', key: 'item1'}]}
+  renderItem={({item}) => <ListItem library={item} />}
+/> */}
+
 
 const mapStateToProps = state => {
     return { libraries: state.libraries };
---
+++ src/components/ListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { CardSection } from './common';
+
+class ListItem extends React.Component {
+    render() {
+        //console.log(this.props.library)
+        return(
+            <CardSection>
+                <Text>{this.props.library.title}</Text>
+            </CardSection>
+        );
+    }
+};
+
+export default ListItem;
